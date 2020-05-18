12e3a1f3807416f090ec96d97b9a5487c09c21fa
lim ji sun <limjisun97@naver.com>
2020-05-18 16:12:45 +0900

    logout and login

--- project1/project1/urls.py
+++ project1/project1/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 import WhatDidYouEat.views
+import accounts.views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,8 @@ urlpatterns = [
     path('edit/<int:pk>', WhatDidYouEat.views.edit, name="edit"),
     path('detail/<int:pk>/delete', WhatDidYouEat.views.delete, name="delete"),
     path('detail/<int:pk>/comment/<int:comment_pk>/delete/', WhatDidYouEat.views.delete_comment,name="delete_comment"),
+    path('registration/register/',accounts.views.register, name="register"),
+    path('registration/',include('django.contrib.auth.urls')),
+    path('registration/login/',auth_views.LoginView.as_view(),{'template_name':'registration/login.html'},name="login"),
+    path('registeration/logged_out/',auth_views.LogoutView.as_view(),{'template_name':'regisration/logged_out.html'},name="logout"),
 ]
